91b4075f1b1fc9182d86b76d4b6b675a4fee0159
Jihui Choi <jhchoi@koost.net>
2018-03-21 15:51:13 +0900

    ADD spell-checker 0.7.1-1

--- Firefox/install.js
+++ Firefox/install.js
@@ -1,4 +1,4 @@
-var err = initInstall("Korean Dictionary", "ko-KR@dictionaries.addons.mozilla.org", "0.6.4-1");
+var err = initInstall("Korean Dictionary", "ko-KR@dictionaries.addons.mozilla.org", "0.6.40.7.10.7.10.7.10.7.1-1");
 if (err != SUCCESS)
     cancelInstall();
 
